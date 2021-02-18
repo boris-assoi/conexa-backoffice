d424a99ec7bcab897d896e27af444d3dfd4717f5
Boris Assoi <boris.assoi@outlook.com>
2021-02-18 17:11:00 +0000

    Ajouts des assets

---
+++ views/inc/assets/css.php
@@ -0,0 +1,19 @@
+<!-- Google fonts -->
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+
+<!-- Icon fonts -->
+<link rel="stylesheet" href="assets/fonts/fontawesome.css">
+<link rel="stylesheet" href="assets/fonts/ionicons.css">
+<link rel="stylesheet" href="assets/fonts/linearicons.css">
+<link rel="stylesheet" href="assets/fonts/open-iconic.css">
+<link rel="stylesheet" href="assets/fonts/pe-icon-7-stroke.css">
+<link rel="stylesheet" href="assets/fonts/feather.css">
+
+<!-- Core stylesheets -->
+<link rel="stylesheet" href="assets/css/bootstrap-material.css">
+<link rel="stylesheet" href="assets/css/shreerang-material.css">
+<link rel="stylesheet" href="assets/css/uikit.css">
+
+<!-- Libs -->
+<link rel="stylesheet" href="assets/libs/perfect-scrollbar/perfect-scrollbar.css">
+<link rel="stylesheet" href="assets/libs/flot/flot.css">
