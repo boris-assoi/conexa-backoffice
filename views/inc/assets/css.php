07387848010898392e97860cfc8b25dce5bf17b8
Boris Assoi <boris.assoi@outlook.com>
2021-02-18 18:03:46 +0000

    Correction des liens

--- views/inc/assets/css.php
+++ views/inc/assets/css.php
@@ -2,18 +2,18 @@
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
 
 <!-- Icon fonts -->
-<link rel="stylesheet" href="assets/fonts/fontawesome.css">
-<link rel="stylesheet" href="assets/fonts/ionicons.css">
-<link rel="stylesheet" href="assets/fonts/linearicons.css">
-<link rel="stylesheet" href="assets/fonts/open-iconic.css">
-<link rel="stylesheet" href="assets/fonts/pe-icon-7-stroke.css">
-<link rel="stylesheet" href="assets/fonts/feather.css">
+<link rel="stylesheet" href="../views/assets/fonts/fontawesome.css">
+<link rel="stylesheet" href="../views/assets/fonts/ionicons.css">
+<link rel="stylesheet" href="../views/assets/fonts/linearicons.css">
+<link rel="stylesheet" href="../views/assets/fonts/open-iconic.css">
+<link rel="stylesheet" href="../views/assets/fonts/pe-icon-7-stroke.css">
+<link rel="stylesheet" href="../views/assets/fonts/feather.css">
 
 <!-- Core stylesheets -->
-<link rel="stylesheet" href="assets/css/bootstrap-material.css">
-<link rel="stylesheet" href="assets/css/shreerang-material.css">
-<link rel="stylesheet" href="assets/css/uikit.css">
+<link rel="stylesheet" href="../views/assets/css/bootstrap-material.css">
+<link rel="stylesheet" href="../views/assets/css/shreerang-material.css">
+<link rel="stylesheet" href="../views/assets/css/uikit.css">
 
 <!-- Libs -->
-<link rel="stylesheet" href="assets/libs/perfect-scrollbar/perfect-scrollbar.css">
-<link rel="stylesheet" href="assets/libs/flot/flot.css">
+<link rel="stylesheet" href="../views/assets/libs/perfect-scrollbar/perfect-scrollbar.css">
+<link rel="stylesheet" href="../views/assets/libs/flot/flot.css">
